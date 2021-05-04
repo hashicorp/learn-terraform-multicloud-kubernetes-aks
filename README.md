7f207536480725309302c57bbe66cb8cae5b7e9e
ritsok <rita@hashicorp.com>
2021-05-04 15:20:10 -0400

    update readme, outputs

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# learn-terraform-multicloud-kubernetes-aks
+# Learn Terraform - Deploy Federated Multi-Cloud Kubernetes Clusters
+
+This is a companion repository with part of the configuration for the [Deploy Federated Multi-Cloud Kubernetes
+Clusters
+guide](https://learn.hashicorp.com/tutorials/terraform/multicloud-kubernetes)
+on [HashiCorp Learn](https://learn.hashicorp.com/). It contains Terraform
+configuration files for you to use to learn how to provision an AKS cluster.
--- outputs.tf
+++ outputs.tf
@@ -7,27 +7,3 @@ output "kubernetes_cluster_name" {
   description = "AKS cluster name"
   value       = azurerm_kubernetes_cluster.default.name
 }
-
-output "host" {
-  description = "AKS endpoint"
-  value       = azurerm_kubernetes_cluster.default.kube_config.0.host
-}
-
-output "client_key" {
-  description = "AKS client key"
-  value       = azurerm_kubernetes_cluster.default.kube_config.0.client_key
-  sensitive   = true
-}
-
-output "client_certificate" {
-  description = "AKS client certificate"
-  value       = azurerm_kubernetes_cluster.default.kube_config.0.client_certificate
-  sensitive   = true
-}
-
-output "cluster_ca_certificate" {
-  description = "AKS cluster CA"
-  value       = azurerm_kubernetes_cluster.default.kube_config.0.cluster_ca_certificate
-  sensitive   = true
-}
-
